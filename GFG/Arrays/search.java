7ca4745ace64509a9bcda5c778c097f9e258ae78
Chandra Bhushan <91384470+bhushanchandra1@users.noreply.github.com>
2023-06-19 16:12:12 +0530

    Add files via upload

---
+++ GFG/Arrays/search.java
@@ -0,0 +1,12 @@
+package GFG.Arrays;
+
+//import java.util.*;
+import java.util.Arrays;
+
+public class search{
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ array/inser.java
@@ -0,0 +1,8 @@
+
+public class inser
+{
+    public static void main(String[] args) {
+       
+    }
+
+}
