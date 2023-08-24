a1136e695a462a86f43b2648af88b2b41f748e3f
Chandra Bhushan <91384470+bhushanchandra1@users.noreply.github.com>
2023-08-24 20:53:51 +0530

    did it

--- recursion_class2/removedup.java
+++ recursion_class2/removedup.java
@@ -1,7 +1,27 @@
 package recursion_class2;
 
-public static boolean arr[] = new boolean[26];
 
 public class removedup {
-       
+    public static boolean arr[] = new boolean[26];
+    public static void remove(String str , int idx , String strnew){
+        if(idx == str.length()){
+            System.out.println(strnew);
+            return;
+        }
+
+        if(arr[str.charAt(idx) - 'a'] == true){
+            remove(str, idx+1, strnew);
+        }
+        else{
+            strnew += str.charAt(idx);
+            arr[str.charAt(idx) - 'a'] = true;
+            remove(str, idx+1, strnew);
+
+        }
+    }
+    public static void main(String[] args) {
+        String str = "aabsdebbs";
+        remove(str, 0, "");
+    }
+
 }
